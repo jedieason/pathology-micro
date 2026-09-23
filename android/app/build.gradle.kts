20fb0661d805184bc08b3e0462036a9ee3c20797
Jedieason <jedieason@gmail.com>
2026-09-23 22:50:36 +0800

    fix(android): bump compileSdk and targetSdk to API 36 (versionCode 2)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,14 +6,14 @@ plugins {
 
 android {
     namespace = "com.jedieason.pathologymicro"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.jedieason.pathologymicro"
         minSdk = 26
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0"
+        targetSdk = 36
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
